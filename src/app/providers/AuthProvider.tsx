98774c3b778aa2507570a3749c3b7f190dad4174
qwerty2944 <qwerty.2944@gmail.com>
2024-10-15 01:03:28 +0900

    feat: Auth 프로바이더추가

---
+++ src/app/providers/AuthProvider.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { onAuthStateChanged, User } from 'firebase/auth';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { auth } from '../../../firebaseApp';
+
+interface AuthContextType {
+  user: User | null;
+  loading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
