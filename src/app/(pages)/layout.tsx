1b5057a0f4b5a3e3efe541a390667a8639acd562
강선우 <74632731+NiNinanana@users.noreply.github.com>
2024-10-15 11:00:26 +0900

    [Feat] 화면 Layout 구분 (#31)
    * design: NavigationMenu shadcn ui 추가
    
    * fix: PATHS 아티클 추가 및 방식 수정
    
    * design: GNB ui 추가
    
    * feat: (pages)에 page.tsx 이동 및 gnb를 활용한 layout 추가
    
    * refactor: menu 내용을 MENU constant 만들어 사용

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react';
+import Gnb from '@/widgets/Gnb/ui/Gnb';
+
+interface PagesLayoutProps {
+  children: ReactNode;
+}
+
+const Pageslayout = ({ children }: PagesLayoutProps) => {
+  return (
+    <div className="flex flex-col gap-10">
+      <Gnb />
+      <div className="flex justify-center px-4">
+        <div className="flex w-full max-w-[1440px] gap-10">
+          {/* TODO: LNB 추가 */}
+          <main>{children}</main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pageslayout;
